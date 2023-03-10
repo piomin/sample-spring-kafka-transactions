272d819e669e282862dacd25e86a550a5d19c501
piomin <piotr.minkowski@gmail.com>
2023-03-10 16:45:04 +0100

    fix tests

--- transactions-service/src/main/java/pl/piomin/services/transactions/repository/OrderGroupRepository.java
+++ transactions-service/src/main/java/pl/piomin/services/transactions/repository/OrderGroupRepository.java
@@ -1,10 +1,10 @@
 package pl.piomin.services.transactions.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import pl.piomin.services.transactions.domain.OrderGroup;
 
-import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface OrderGroupRepository extends
